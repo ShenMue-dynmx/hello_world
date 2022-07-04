ff7ef0bafba16755338ccf55b46df6518ef442f6
ShenMU <primulcouncil@gmail.com>
2022-07-04 19:58:20 +0300

    added glfw

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 #include "adder.h"
+#include <GLFW/glfw3.h>
+
 int main(){
     std::cout<<"Fucco wacco \n"<<add(420.0f,360.0f)<<std::endl;
+
+    GLFWwindow *window;
+    if(!glfwInit())
+    {
+        fprintf(stderr,"Failed to initialize GLFW\n");
+        exit(EXIT_FAILURE);
+    }
+    window = glfwCreateWindow( 300, 300, "Gears", NULL, NULL );
+    while( !glfwWindowShouldClose(window) )
+    {
+        // Swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
     return 0;
 }
